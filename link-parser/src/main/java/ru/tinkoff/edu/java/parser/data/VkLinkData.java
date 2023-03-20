6aa8bb503eb38a778d9ad7b40d2367aedf0d1c99
Manylovvv <feed.lastik@gmail.com>
2023-03-20 18:15:16 +0500

    added links for parsing

--- link-parser/src/main/java/ru/tinkoff/edu/java/parser/data/VkLinkData.java
+++
@@ -1,9 +0,0 @@
-package ru.tinkoff.edu.java.parser.data;
-
-public record VkLinkData(String profile_id) implements LinkData {
-    @Override
-    public String toString() {
-        return profile_id;
-    }
-
-}
