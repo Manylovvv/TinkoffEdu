aafedf312d6ab9ae6b8ac4e72e5320f65b916a9d
Manylovvv <feed.lastik@gmail.com>
2023-04-03 01:24:56 +0500

    добавлена проверка работы клиентов

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -3,6 +3,13 @@ package ru.tinkoff.edu.java.scrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.tinkoff.edu.java.parser.data.GitHubLinkData;
+import ru.tinkoff.edu.java.parser.data.StackOverflowLinkData;
+import ru.tinkoff.edu.java.scrapper.api.client.HttpGitHubClient;
+import ru.tinkoff.edu.java.scrapper.api.client.HttpStackOverflowClient;
+import ru.tinkoff.edu.java.scrapper.api.response.github.GitHubApiResponse;
+import ru.tinkoff.edu.java.scrapper.api.response.stackoverflow.StackOverflowItemApiResponse;
 import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
 
 @SpringBootApplication
@@ -12,5 +19,16 @@ public class ScrapperApplication {
                 var ctx = SpringApplication.run(ScrapperApplication.class, args);
                 ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
                 System.out.println(config);
+                WebClient webClient = WebClient.create();
+
+                HttpGitHubClient gitHubClient = new HttpGitHubClient(webClient);
+                GitHubLinkData userAndRepoGit = new GitHubLinkData("Manylovvv", "TinkoffEdu");
+                GitHubApiResponse response = gitHubClient.GetRepo(userAndRepoGit);
+
+                System.out.println(response);
+                HttpStackOverflowClient stackOverflowClient = new HttpStackOverflowClient(webClient);
+                StackOverflowLinkData userAndRepoStack = new StackOverflowLinkData("1642028");
+                StackOverflowItemApiResponse resp =  stackOverflowClient.GetQuestion(userAndRepoStack);
+                System.out.println(resp);
         }
 }
