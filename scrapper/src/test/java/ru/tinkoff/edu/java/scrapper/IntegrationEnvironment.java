e01e0470888c1ae7b6ad32ba47a29b64761c2e70
Manylovvv <feed.lastik@gmail.com>
2023-04-11 14:57:15 +0500

    package for test was established

--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/IntegrationEnvironment.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/IntegrationEnvironment.java
@@ -1,4 +1,4 @@
-package edu;
+package ru.tinkoff.edu.java.scrapper;
 
 import liquibase.Contexts;
 import liquibase.LabelExpression;
--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/IntegrationTest.java
@@ -1,4 +1,4 @@
-package edu;
+package ru.tinkoff.edu.java.scrapper;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
