242d8d3bc927d20b208352d5574ce3a37f83770f
Manylovvv <feed.lastik@gmail.com>
2023-05-17 02:49:30 +0500

    test was refactored

--- bot/src/test/java/ru/tinkoff/edu/java/java/bot/CoreUpdaterTest.java
+++ bot/src/test/java/ru/tinkoff/edu/java/java/bot/CoreUpdaterTest.java
@@ -4,10 +4,7 @@ import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collections;
-import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.tinkoff.edu.java.java.bot.controller.dto.response.LinkResponse;
 import ru.tinkoff.edu.java.java.bot.controller.dto.response.ListLinksResponse;
 import ru.tinkoff.edu.java.java.bot.scrapper.api.ScrapperClient;
 import ru.tinkoff.edu.java.java.bot.telegram.command.impl.ListCommand;
@@ -33,7 +29,7 @@ public class CoreUpdaterTest {
     @Mock
     private Message message;
     @Mock
-    private Chat chat;
+    private Chat tgchat;
     private CoreUpdater messageProcessor;
 
     @BeforeEach
@@ -42,36 +38,30 @@ public class CoreUpdaterTest {
     }
 
     @Test
-    public void processInvalidCommand() {
-        Long chatId = 123456L;
-
+    public void processListCommand_emptyList() {
+        Long chatId = 43215784L;
+        String emptyListMessage = "Список отслеживаемых ссылок пуст!";
+        ListLinksResponse response = new ListLinksResponse(Collections.emptyList(), 0);
         when(update.message()).thenReturn(message);
-        when(message.text()).thenReturn("/invalid");
-        when(message.chat()).thenReturn(chat);
-        when(chat.id()).thenReturn(chatId);
-
-        SendMessage message = messageProcessor.processCommand(update);
-
-        Assertions.assertEquals(chatId, message.getParameters().get("chat_id"));
-        Assertions.assertEquals("Неизвестная команда, введите /help " +
-            "для просмотрадоступных команд!", message.getParameters().get("text"));
+        when(message.text()).thenReturn("/list");
+        when(message.chat()).thenReturn(tgchat);
+        when(tgchat.id()).thenReturn(chatId);
+        when(client.getListLinks(anyLong())).thenReturn(response);
+        SendMessage message1 = messageProcessor.processCommand(update);
+        Assertions.assertEquals(chatId, message1.getParameters().get("chat_id"));
+        Assertions.assertEquals(emptyListMessage, message1.getParameters().get("text"));
     }
 
     @Test
-    public void processListCommand_emptyList() {
-        Long chatId = 12345678L;
-        String emptyListMessage = "Список отслеживаемых ссылок пуст!";
-        ListLinksResponse response = new ListLinksResponse(Collections.emptyList(), 0);
-
+    public void processInvalidCommand() {
+        Long chatId = 321321L;
         when(update.message()).thenReturn(message);
-        when(message.text()).thenReturn("/list");
-        when(message.chat()).thenReturn(chat);
-        when(chat.id()).thenReturn(chatId);
-        when(client.getListLinks(anyLong())).thenReturn(response);
-
+        when(message.text()).thenReturn("/invalid");
+        when(message.chat()).thenReturn(tgchat);
+        when(tgchat.id()).thenReturn(chatId);
         SendMessage message = messageProcessor.processCommand(update);
-
-        Assertions.assertEquals(emptyListMessage, message.getParameters().get("text"));
         Assertions.assertEquals(chatId, message.getParameters().get("chat_id"));
+        Assertions.assertEquals("Неизвестная команда, введите /help " +
+            "для просмотрадоступных команд!", message.getParameters().get("text"));
     }
 }
